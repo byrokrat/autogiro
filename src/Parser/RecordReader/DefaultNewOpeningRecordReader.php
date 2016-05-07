3d13bda02899577c9212f0d0c6e2d84ce5d72623
Hannes Forsgård <hannes.forsgard@fripost.org>
2016-05-07 16:57:19 +0200

    Add copyright notice

--- src/Parser/RecordReader/DefaultNewOpeningRecordReader.php
+++ src/Parser/RecordReader/DefaultNewOpeningRecordReader.php
@@ -1,6 +1,24 @@
 <?php
+/**
+ * This file is part of byrokrat/autogiro.
+ *
+ * byrokrat/autogiro is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * byrokrat/autogiro is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with byrokrat/autogiro. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2016 Hannes Forsgård
+ */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace byrokrat\autogiro\Parser\RecordReader;
 
@@ -35,7 +53,7 @@ class DefaultNewOpeningRecordReader extends RecordReader
                 'customerNr' => new Matcher\Number(65, 6),
                 'bankgiro' => new Matcher\Number(71, 10),
             ],
-            function (array $values) use ($accountFactory) {
+            function(array $values) use ($accountFactory) {
                 return new Record\OpeningRecord(
                     trim($values['layout']),
                     \DateTimeImmutable::createFromFormat('Ymd', $values['date']),
